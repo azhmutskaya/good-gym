934f8f99a9d0c33ad70c800b9074a9b96db4c82d
alexandra <alexandra.zhmutskaya@pactera.com>
2019-07-18 17:07:24 +0300

    search fe

--- src/app/+admin/modules/clients/components/search/search.component.ts
+++ src/app/+admin/modules/clients/components/search/search.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  formIsVisible = false;
+  filterIsActive = false;
+
   constructor() {
   }
 
