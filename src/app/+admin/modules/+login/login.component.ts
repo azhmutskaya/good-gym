855bc4a8e9106dc0b1ad40a1540685db930a106b
alexandra <alexandra.zhmutskaya@pactera.com>
2019-07-23 12:30:59 +0300

    login

--- src/app/+admin/modules/+login/login.component.ts
+++ src/app/+admin/modules/+login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  passwordIsVisible = false;
+
+  error = false;
+
+  loginForm = this.fb.group({
+    userName: [null, Validators.required],
+    password: [null, Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {
   }
 
   ngOnInit() {
+
+  }
+
+  trimField([key, index]: [string, number?]): void {
+    const control = (index !== undefined)
+      ? this.loginForm.get(key).get(`${index}`)
+      : this.loginForm.get(key);
+
+    if (typeof control.value !== 'object') {
+      control.setValue(control.value.trim());
+    }
+  }
+
+  isFilled([key, index]: [string, number?]): boolean {
+    const control = (index !== undefined)
+      ? this.loginForm.get(key).get(`${index}`)
+      : this.loginForm.get(key);
+
+    return (typeof control.value !== 'object') && control.value.trim();
+  }
+
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      if (this.loginForm.get('userName').value  === 'Admin' && this.loginForm.get('password').value === 'Admin') { // facepalm
+        console.log('login');
+        this.error = false;
+      } else {
+        this.error = true;
+      }
+    }
+  }
+
+  togglePassword($password) {
+    this.passwordIsVisible = !this.passwordIsVisible;
+    if (this.passwordIsVisible) {
+      $password.setAttribute('type', 'text');
+    } else {
+      $password.setAttribute('type', 'password');
+    }
   }
 
 }
--- src/app/+admin/modules/+login/login.module.ts
+++ src/app/+admin/modules/+login/login.module.ts
@@ -4,13 +4,15 @@ import { LoginRoutingModule } from './login-routing.module';
 
 import { LoginComponent } from './login.component';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    IconSpriteModule
+    IconSpriteModule,
+    ReactiveFormsModule,
   ]
 })
 export class LoginModule {
