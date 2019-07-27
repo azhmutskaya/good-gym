a1ae85ffee9746ca2905a81c8c5427011af4c541
alexandra <alexandra.zhmutskaya@pactera.com>
2019-07-27 18:54:54 +0300

    part 1

--- src/app/+frontend/frontend.component.ts
+++ src/app/+frontend/frontend.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontendComponent implements OnInit {
 
+  config: any;
+  fullpageApi: any;
+
   constructor() {
   }
 
+  getRef(fullPageRef) {
+    this.fullpageApi = fullPageRef;
+  }
+
   ngOnInit() {
+    this.config = {
+      //licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['home', 'about', 'workout', 'plans', 'interior', 'contacts', 'map', 'sign-up', ''],
+      lazyLoading: true,
+      slidesNavigation: true,
+      responsiveHeight: 420,
+      //keyboardScrolling: false,
+      touchSensitivity: 17,
+      afterLoad: (origin, destination) => {
+        destination.item.classList.add('is-animated');
+      },
+      afterSlideLoad: (section, origin, destination) => {
+        destination.item.classList.add('is-animated');
+      }
+    };
   }
 
 }
--- src/app/+frontend/frontend.module.ts
+++ src/app/+frontend/frontend.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrontendComponent } from './frontend.component';
 import { FrontendRoutingModule } from './frontend-routing.module';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @NgModule({
   declarations: [FrontendComponent],
   imports: [
     CommonModule,
-    FrontendRoutingModule
+    FrontendRoutingModule,
+    AngularFullpageModule,
+    IconSpriteModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class FrontendModule {
